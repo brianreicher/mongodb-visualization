398d3ae86b981536e47ef17069cea00dae992ee6
brianreicher <reicher.b@northeastern.edu>
2023-02-22 16:29:58 -0500

    Updating mongo connection

---
+++ src/mongo_connection.py
@@ -0,0 +1,68 @@
+import pymongo
+import json
+from typing import List, Dict
+
+
+class MongoConnector:
+    """
+    A class to connect to a MongoDB server and perform CRUD operations.
+
+    Attributes:
+        host (str): The hostname of the MongoDB server.
+        port (int): The port number of the MongoDB server.
+        db_name (str): The name of the MongoDB database.
+
+    """
+
+    def __init__(self, host: str, port: int, db_name: str) -> None:
+        """
+        Constructs a new MongoConnector object.
+
+        Args:
+            host (str): The hostname of the MongoDB server.
+            port (int): The port number of the MongoDB server.
+            db_name (str): The name of the MongoDB database.
+
+        """
+        self.host: str = host
+        self.port: int = port
+        self.db_name: str = db_name
+        self.client = None
+        self.db = None
+
+    def connect(self) -> None:
+        """
+        Connects to the MongoDB server.
+
+        """
+        self.client: pymongo.MongoClient = pymongo.MongoClient(host=self.host, port=self.port)
+        self.db = self.client[self.db_name]
+
+    def disconnect(self) -> None:
+        """
+        Disconnects from the MongoDB server.
+
+        """
+        self.client.close()
+
+    def insert_data(self, collection_name: str, json_file: str) -> None:
+        """
+        Inserts data from a JSON file into a MongoDB collection.
+
+        Args:
+            collection_name (str): The name of the MongoDB collection.
+            json_file (str): The path to the JSON file.
+
+        """
+        with open(json_file) as f:
+            data: List[Dict] = json.load(f)
+            collection = self.db[collection_name]
+            collection.insert_many(data)
+        print(f"{len(data)} documents inserted into collection {collection_name}.")
+
+
+if __name__ == '__main__':
+    mongo: MongoConnector = MongoConnector('localhost', 27017, 'restaurants')
+    mongo.connect()
+    mongo.insert_data('restaurants_db', '/Users/brianreicher/Downloads/restaurants.json')
+    mongo.disconnect()
