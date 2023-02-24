89b219ad1ce6512f95c9579d4ffbe35f26436a38
brianreicher <reicher.b@northeastern.edu>
2023-02-24 10:32:54 -0500

    Touching up driver and notebook

--- src/mongo_connection.py
+++ src/mongo_connection.py
@@ -36,9 +36,12 @@ class MongoDriver():
         """
         Connects to the MongoDB server.
         """
-        self.client: pymongo.MongoClient = pymongo.MongoClient(host=self.host, port=self.port)
-        self.db = self.client[self.db_name]
-        print(self.db)
+        try:
+            self.client: pymongo.MongoClient = pymongo.MongoClient(host=self.host, port=self.port)
+            self.db = self.client[self.db_name]
+            print(self.db)
+        except pymongo.errors.ConnectionFailure as e:
+            print(f"Failed to connect to MongoDB server: {e}")
 
     def disconnect(self) -> None:
         """
@@ -214,127 +217,3 @@ class MongoDriver():
         fig.show()
         if save_as is not None:
             pio.write_image(fig, save_as)
-
-
-if __name__ == '__main__':
-    mongo: MongoDriver = MongoDriver('localhost', 27017, 'restaurants')
-    mongo.connect()
-
-    if mongo.collection_size("restaurants_collection") == 0:
-        mongo.insert_data('restaurants_collection', 'data/restaurants.json', clear=False)
-
-    print("\n Number of McDonald's in NYC: \n")
-    mongo.aggregate_query("restaurants_collection", [
-                                                    {
-                                                        "$match": {
-                                                                    "name":"Mcdonald'S"
-                                                                    }
-                                                    },
-                                                    {"$count":"totalMcDonalds"}
-                                                    ])
-
-    print("\n Number of restaurants in each bourough: \n")
-    mongo.aggregate_query("restaurants_collection", [
-                                                    {
-                                                        '$group': {
-                                                            '_id': '$borough',
-                                                            'count': {'$sum': 1}
-                                                        }
-                                                    }
-                                                ])
-
-    print("\n Boroughs with the highest number of Chinese restaurants --> give the number of Chinese restaurants in each boroughough: \n")
-    mongo.aggregate_query("restaurants_collection", [
-                                                    { "$match": { "cuisine": "Chinese" } },
-                                                    {
-                                                        "$group": {
-                                                        "_id": "$borough",
-                                                        "count": { "$sum": 1 }
-                                                        }
-                                                    },
-                                                    { "$sort": { "count": -1 } }
-                                                    ])
-    
-    print("\n Top 10 restaurants with the highest average score, sorted by average score (min 5 reviews): \n")
-    mongo.aggregate_query("restaurants_collection", 
-                                                       [{"$match": {"grades":{"$gt": 5}}},
-                                                        {"$project": {
-                                                            "name": 1,
-                                                            "borough": 1,
-                                                            "cuisine": 1,
-                                                            "grades": 1,
-                                                            "avgScore": {"$avg": "$grades.score"}
-                                                        }},
-                                                        {"$sort": {"avgScore": -1}},
-                                                        {"$limit": 10}
-                                                    ])
-    
-    print("\n Restaurants which have a zipcode that starts with '10' and they are of either Italian or Chinese cuisine and have been graded 'A' in their latest grade: \n")
-    mongo.search_query(collection_name="restaurants_collection", qu = {
-                                                            "address.zipcode": { "$regex": "^10" },
-                                                            "$or": [
-                                                                { "cuisine": "Italian" },
-                                                                { "cuisine": "Chinese" }
-                                                            ],
-                                                            "grades.0.grade": "A"
-                                                        },
-                                                proj = {
-                                                    "name": 1,
-                                                    "borough": 1,
-                                                    "cuisine": 1,
-                                                    "grades.date": 1,
-                                                    "grades.grade": 1,
-                                                    "grades.score": 1
-                                                })
-    
-    print("\n All restaurants that are located in the Bronx borough and have an 'American' cuisine: \n")
-    mongo.search_query(collection_name="restaurants_collection", qu = {"borough": "Bronx", "cuisine": "American"},
-                       proj={ "_id": 0,
-                              "name": 1,
-                              "borough": 1,
-                              "cuisine": 1}, lim=5)
-
-    print("\n All restaurants that have a 'Pizza' cuisine and a 'B' grade in their latest inspection: \n")
-    mongo.search_query(collection_name="restaurants_collection",     qu={
-                                                                        "cuisine": "Pizza",
-                                                                        "grades.0.grade": "B"
-                                                                    },
-                                                                    proj={
-                                                                        "_id": 0,
-                                                                        "name": 1,
-                                                                        "borough": 1,
-                                                                        "cuisine": 1,
-                                                                        "grades": {
-                                                                            "$elemMatch": {"grade": "B"}
-                                                                        }
-                                                                    }, lim=5)
-
-    print("\n Top 5 restaurants with the highest average score for their grades, and only show their name, cuisine, and average score: \n")
-    mongo.search_query(collection_name="restaurants_collection",qu={}, proj={"name": 1, "cuisine": 1, "avgScore": {"$avg": "$grades.score"}, "_id": 0}, lim=5)
-
-
-    print("\n All restaurants that have a 'Chinese' or 'Japanese' cuisine, and are located in the 'Queens' borough, and have a grade 'A' in their latest inspection. \n")
-    mongo.search_query(collection_name="restaurants_collection",     qu={
-                                                                        "borough": "Queens", 
-                                                                        "cuisine": {"$in": ["Chinese", "Japanese"]}, 
-                                                                        "grades.0.grade": "A"
-                                                                    }, 
-                                                                    proj={
-                                                                        "name": 1, 
-                                                                        "borough": 1, 
-                                                                        "cuisine": 1, 
-                                                                        "grades": 1,
-                                                                        "_id": 0
-                                                                    },
-                                                                    lim=5)
-
-    print("\n Visualization of the distribution of restaurants across different cuisines in the NYC boroughs: \n")
-    res: list = mongo.aggregate_query("restaurants_collection", [{"$group": {"_id": {"borough": "$borough", "cuisine": "$cuisine"}, "count": {"$sum": 1}}},
-                                                                    {"$project": {"borough": "$_id.borough", "cuisine": "$_id.cuisine", "count": "$count", "_id": 0}}]
-                                                                    , show=False)
-
-    # plot the previous query
-    mongo.plot_query(res, x_var="borough", y_var="count", color_on="cuisine", plot_title="Resturant Count by Cusiene in NYC Boroughs", save_as='./data/mongo_visualization.png')
-
-    # mongo.flush_collection("restaurants_collection")
-    mongo.disconnect()
